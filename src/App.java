f0c23a4e0d1b101852333f667ebb59379450fdac
lucasaraujo6 <lucas_araujo_11@hotmail.com>
2022-02-11 00:21:47 -0300

    Finalizado

--- src/App.java
+++ src/App.java
@@ -1,54 +1,88 @@
 import java.util.Scanner;
 
-import javax.swing.plaf.synth.ColorType;
+public class App 
+{
+    static int valorMax, qntdColunas, qntdLinhas, colUltimaAlteracao;
+    static int[][] valores;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
-        int valorMax = scan.nextInt();
-        int qntdColunas = scan.nextInt();
-        int qntdLinhas = (int) Math.pow(valorMax, qntdColunas);
-        int colUltimaAlteracao = qntdColunas-1;
-        // System.out.println("qntd linhas"+qntdLinhas);
-        int[][] valores = new int[qntdLinhas][qntdColunas];
+    public static void main(String[] args) throws Exception 
+    {
+        getValues();
+        setValues();
+        fillVector();
+        imprimir();
+    }
 
-        // Define o primeiro termo 1111111
-        for (int i = 0; i < qntdColunas; i++) {
+    private static void imprimir() 
+    {
+        for (int i = 0; i < qntdLinhas; i++) 
+        {
+            for (int j = 0; j < qntdColunas; j++) 
+            {
+                System.out.print(valores[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+    }
 
+    private static void fillVector() 
+    {
+        // primeira linha
+        for (int i = 0; i < qntdColunas; i++) 
+        {
             valores[0][i] = 1;
         }
-        // System.out.println("valores primeira: ");
-        // for (int i=0;i<qntdColunas;i++){
-        //     System.out.print(valores[0][i]);
-        // }
-        // System.out.println("---------------");
-        for (int i = 1; i < qntdLinhas; i++) {
-            for (int k = 0; k < qntdColunas; k++) {
+        // demais linhas
+        for (int i = 1; i < qntdLinhas; i++) 
+        {
+            for (int k = 0; k < qntdColunas; k++) 
+            {
                 valores[i][k] = valores[i - 1][k];
             }
-            for (int j = qntdColunas - 1; j > -1; j--) {
-            
-                if (valores[i - 1][j] < valorMax) {
-
-                    if (j<colUltimaAlteracao){
-                        for (int k = j; k < qntdColunas; k++) {
+            for (int j = qntdColunas - 1; j > -1; j--) 
+            {
+                if (valores[i - 1][j] < valorMax) 
+                {
+                    if (j < colUltimaAlteracao) 
+                    {
+                        for (int k = j; k < qntdColunas; k++) 
+                        {
                             valores[i][k] = 1;
                         }
                     }
-                    colUltimaAlteracao=j;
+                    colUltimaAlteracao = j;
                     valores[i][j] = valores[i - 1][j] + 1;
                     break;
                 }
-            }        
-        }
-        for (int i = 0; i < qntdLinhas; i++) {
-
-            for (int j = 0; j < qntdColunas; j++) {
-                System.out.print(valores[i][j]);
             }
-            System.out.println("");
-
         }
+    }
 
+    private static void setValues() 
+    {
+        qntdLinhas = (int) Math.pow(valorMax, qntdColunas);
+        colUltimaAlteracao = qntdColunas - 1;
+        valores = new int[qntdLinhas][qntdColunas];
+    }
+
+    private static void getValues() 
+    {
+        Scanner scan = new Scanner(System.in);
+        do 
+        {
+            try 
+            {
+                System.out.println("Digite, respectivamente, os valores de r e n:");
+                qntdColunas = scan.nextInt();
+                valorMax = scan.nextInt();
+                
+            } catch (Exception e) 
+            {
+                System.out.println("Valores inválidos!");
+                scan.nextLine();
+            }
+        } while (2 > qntdColunas || qntdColunas > 10 || valorMax > 20 || 1 > valorMax);
+
+        scan.close();
     }
 }
